ec049b7cf31e03fd3dcf44d401d49345d6f98355
Victor Bersy <victorbersy+github@gmail.com>
2014-05-10 18:27:12 +0200

    update for Jarvis version 2.0.0 release

--- init.rb
+++ init.rb
@@ -1,13 +1,16 @@
 require 'twitter'
 require 'yaml'
 class TwitterClient
-  def self.init
-    keys = YAML.load_file(File.join(File.dirname(__FILE__), 'config', 'config.yml'))
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = keys['api_key']
-      config.consumer_secret     = keys['api_secret']
-      config.access_token        = keys['access_token']
-      config.access_token_secret = keys['access_token_secret']
+  def initialize
+    @keys = YAML.load_file(File.join(File.dirname(__FILE__), 'config', 'config.yml'))
+  end
+
+  def client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key        = @keys['api_key']
+      config.consumer_secret     = @keys['api_secret']
+      config.access_token        = @keys['access_token']
+      config.access_token_secret = @keys['access_token_secret']
     end
   end
 end
